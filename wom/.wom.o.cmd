0329a5d7c4788137bbbd0a4bc774d9dc12265969
derAlteBarbarossa <57541614+derAlteBarbarossa@users.noreply.github.com>
2021-02-16 11:20:49 +0100

    Delete .wom.o.cmd

--- wom/.wom.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/shahriar/Desktop/Main/Kaveh/TA/lab3_meltdown/lab3/wom/wom.o := ld -m elf_x86_64  -z max-page-size=0x200000    -r -o /home/shahriar/Desktop/Main/Kaveh/TA/lab3_meltdown/lab3/wom/wom.o /home/shahriar/Desktop/Main/Kaveh/TA/lab3_meltdown/lab3/wom/main.o
